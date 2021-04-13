e855038b151af0d9e0d95c35586169ae974adfc6
jiangwentao <jiangwentao@xylink.com>
2021-04-13 20:13:33 +0800

    adjust

--- Spring/src/main/java/service/HelloBeanPostProcessor.java
+++ Spring/src/main/java/service/HelloBeanPostProcessor.java
@@ -5,6 +5,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author jiangwentao
+ */
 @Component
 public class HelloBeanPostProcessor implements BeanPostProcessor {
     @Override
--- springboot/src/main/java/com/jwt/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/jwt/springboot/SpringbootApplication.java
@@ -3,6 +3,9 @@ package com.jwt.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author jiangwentao
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
