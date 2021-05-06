070f97c8a3b5c3aa068da5cf4aa24a1c41db33f8
jiangwentao <jiangwentao@xylink.com>
2021-05-06 19:41:55 +0800

    剑指offer: 解题4

---
+++ LEETCODE/Sward_To_Offer/Solution4.java
@@ -0,0 +1,51 @@
+package Sward_To_Offer;
+
+import java.util.Stack;
+
+/**
+ * @author jiangwentao
+ * @date 2021/5/6
+ * @detail 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 10000
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution4 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public int[] reversePrint(ListNode head) {
+
+        Stack<Integer> stack = new Stack<>();
+        ListNode listNode = head;
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        int[] result = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            result[i++] = stack.pop();
+        }
+        return result;
+    }
+}
