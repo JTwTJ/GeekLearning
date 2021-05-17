9abd1fa303f3eec6b003c57dcd44c624a8387d8c
jiangwentao <jiangwentao@xylink.com>
2021-05-17 20:09:54 +0800

    剑指offer: 14

---
+++ LEETCODE/Sword_To_Offer/Solution14.java
@@ -0,0 +1,55 @@
+package Sword_To_Offer;
+
+/**
+ * @author jiangwentao
+ * @date 2021/5/17
+ * @detail 请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。
+ * 例如，把 9表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011中，共有三位为 '1'。
+ * 示例 2：
+ * <p>
+ * 输入：00000000000000000000000010000000
+ * 输出：1
+ * 解释：输入的二进制串 00000000000000000000000010000000中，共有一位为 '1'。
+ * 示例 3：
+ * <p>
+ * 输入：11111111111111111111111111111101
+ * 输出：31
+ * 解释：输入的二进制串 11111111111111111111111111111101中，共有 31 位为 '1'。
+ * <p>
+ * 提示：
+ * <p>
+ * 输入必须是长度为 32 的 二进制串 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution14 {
+
+    // you need to treat n as an unsigned value
+    //二进制数与1 与操作可判断最右一位是0还是1， 然后通过无符号右移统计下一位
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution14 solution14 = new Solution14();
+        int i = solution14.hammingWeight(5);
+        System.out.println(i);
+        System.out.println(8 << 1);
+    }
+}
