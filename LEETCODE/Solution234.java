8551b6ec59c1db9041df90c0b31c1d0654483b4c
jiangwentao <jiangwentao@xylink.com>
2021-05-25 20:31:26 +0800

    leetcode: 判断是否为回文链表

---
+++ LEETCODE/Solution234.java
@@ -0,0 +1,117 @@
+
+
+/**
+ * @author jiangwentao
+ * @date 2021/5/24
+ * @detail 回文链表
+ * 请判断一个链表是否为回文链表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * <p>
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution234 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    ListNode left;
+
+    //利用调用栈进行回文链表判断
+    public boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    /**
+     * 利用调用栈比较
+     *
+     * @param right 右
+     * @return 链表现头结点
+     */
+    boolean traverse(ListNode right) {
+
+        if (right == null) {
+            return true;
+        }
+        boolean res = traverse(right.next);
+        res = res && (left.val == right.val);
+        left = left.next;
+        return res;
+    }
+
+    /**
+     * 快慢指针实现回文链表判断
+     *
+     * @param head 头节点
+     * @return 是否为回文链表
+     */
+    public boolean isPalindrome1(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode newHead = reverse(slow);
+        while (head != null && newHead != null) {
+            if (head.val != newHead.val) {
+                return false;
+            }
+            head = head.next;
+            newHead = newHead.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+        Solution234 solution234 = new Solution234();
+        boolean palindrome = solution234.isPalindrome1(head);
+        System.out.println(palindrome);
+    }
+}
