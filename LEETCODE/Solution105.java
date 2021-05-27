cc9d19c6c02d8955af46857b274c5677bc5c0258
jiangwentao <jiangwentao@xylink.com>
2021-05-27 20:07:58 +0800

    leetcode: 二叉树练习

---
+++ LEETCODE/Solution105.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jiangwentao
+ * @date 2021/5/27
+ * @detail 从前序与中序遍历序列构造二叉树
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * <p>
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * <p>
+ * 例如，给出
+ * <p>
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution105 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private Map<Integer, Integer> valPosMap = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            valPosMap.put(inorder[i], i);
+        }
+        return dfs(preorder, 0, preorder.length - 1, 0);
+    }
+
+    private TreeNode dfs(int[] preorder, int start, int end, int rootIndex) {
+
+        //base case
+        if (start > end) {
+            return null;
+        }
+        int rootVal = preorder[rootIndex];
+        //求rootVal在中序遍历处的位置
+        Integer position = valPosMap.get(rootVal);
+        TreeNode root = new TreeNode(rootVal);
+        root.left = dfs(preorder, start, position - 1, rootIndex + 1);
+        root.right = dfs(preorder, position + 1, end, rootIndex + (position - start) + 1);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution105 solution105 = new Solution105();
+        int[] preOrder = {1, 2, 3};
+        int[] inOrder = {3, 2, 1};
+        TreeNode treeNode = solution105.buildTree(preOrder, inOrder);
+        System.out.println(treeNode);
+    }
+}
