91d0219ba312b4d4cd57ac1ab0f9376e53bb4c1b
jiangwentao <jiangwentao@xylink.com>
2021-05-27 20:20:46 +0800

    leetcode: 二叉树练习

--- LEETCODE/Solution106.java
+++ LEETCODE/Solution106.java
@@ -4,16 +4,16 @@ import java.util.Map;
 /**
  * @author jiangwentao
  * @date 2021/5/27
- * @detail 从前序与中序遍历序列构造二叉树
- * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * @detail 从中序与后序遍历序列构造二叉树
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
  * <p>
  * 注意:
  * 你可以假设树中没有重复的元素。
  * <p>
  * 例如，给出
  * <p>
- * 前序遍历 preorder = [3,9,20,15,7]
  * 中序遍历 inorder = [9,3,15,20,7]
+ * 后序遍历 postorder = [9,15,7,20,3]
  * 返回如下的二叉树：
  * <p>
  * 3
@@ -24,11 +24,10 @@ import java.util.Map;
  * <p>
  * <p>
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Solution105 {
-
+public class Solution106 {
     public class TreeNode {
         int val;
         TreeNode left;
@@ -49,34 +48,25 @@ public class Solution105 {
     }
 
     private Map<Integer, Integer> valPosMap = new HashMap<>();
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
         for (int i = 0; i < inorder.length; i++) {
             valPosMap.put(inorder[i], i);
         }
-        return dfs(preorder, 0, inorder.length - 1, 0);
+        return dfs(postorder, 0, inorder.length - 1, inorder.length - 1);
     }
 
-    private TreeNode dfs(int[] preorder, int start, int end, int rootIndex) {
+    private TreeNode dfs(int[] postOrder, int start, int end, int rootIndex) {
 
         //base case
         if (start > end) {
             return null;
         }
-        int rootVal = preorder[rootIndex];
+        int rootVal = postOrder[rootIndex];
         //求rootVal在中序遍历处的位置
         Integer position = valPosMap.get(rootVal);
         TreeNode root = new TreeNode(rootVal);
-        root.left = dfs(preorder, start, position - 1, rootIndex + 1);
-        root.right = dfs(preorder, position + 1, end, rootIndex + (position - start) + 1);
+        root.left = dfs(postOrder, start, position - 1, rootIndex - (end - position) -1);
+        root.right = dfs(postOrder, position + 1, end, rootIndex - 1);
         return root;
-    }
-
-    public static void main(String[] args) {
-        Solution105 solution105 = new Solution105();
-        int[] preOrder = {1, 2, 3};
-        int[] inOrder = {3, 2, 1};
-        TreeNode treeNode = solution105.buildTree(preOrder, inOrder);
-        System.out.println(treeNode);
     }
 }
