04eeda87683dc1d2f5ff29b015fa27778e92d55c
jiangwentao <jiangwentao@xylink.com>
2021-05-27 16:37:42 +0800

    leetcode: 链表算法题练习

---
+++ LEETCODE/Sword_To_Offer/Solution23.java
@@ -0,0 +1,78 @@
+package Sword_To_Offer;
+
+/**
+ * @author jiangwentao
+ * @date 2021/5/27
+ * @detail 合并两个排序的链表
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * <p>
+ * 示例1：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 1000
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution23 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    //伪头节点
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode(0);
+        ListNode cur = newHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return newHead.next;
+    }
+
+    //递归调用
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        return recur(l1, l2);
+    }
+
+    private ListNode recur(ListNode l1, ListNode l2) {
+
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode cur = null;
+        if (l1.val < l2.val) {
+            cur = new ListNode(l1.val);
+            cur.next = recur(l1.next, l2);
+        } else {
+            cur = new ListNode(l2.val);
+            cur.next = recur(l1, l2.next);
+        }
+        return cur;
+    }
+}
