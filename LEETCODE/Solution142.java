832565917d9ac16d2be22061d51d434101fd839d
jiangwentao <jiangwentao@xylink.com>
2021-05-28 11:18:09 +0800

    leetcode: 链表

---
+++ LEETCODE/Solution142.java
@@ -0,0 +1,85 @@
+/**
+ * @author jiangwentao
+ * @date 2021/5/28
+ * @detail 环形链表 II
+ * 给定一个链表，返回链表开始入环的第一个节点。如果链表无环，则返回null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ * <p>
+ * 进阶：
+ * <p>
+ * 你是否可以使用 O(1) 空间解决此题？
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：返回索引为 0 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：返回 null
+ * 解释：链表中没有环。
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目范围在范围 [0, 104] 内
+ * -105 <= Node.val <= 105
+ * pos 的值为 -1 或者链表中的一个有效索引
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution142 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    //快慢双指针，如果链表有环那么快慢指针会相遇，
+    // 相遇过后将其中一个节点重新置为head节点，再次相遇所在节点就为环开始节点
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        //找到循环链表相遇点
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        slow = head;
+        while (slow != fast && fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        return fast;
+    }
+}
