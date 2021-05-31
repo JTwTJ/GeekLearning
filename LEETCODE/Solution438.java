a1121799b79b8878cefb03ea4f4ea0ca353cb701
JtwtJ <2324302816@qq.com>
2021-06-01 00:05:31 +0800

    leetcode: 滑动窗口框架练习

---
+++ LEETCODE/Solution438.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: msi-
+ * Date: 2021/5/31
+ * Time: 23:46
+ * Description: 找到字符串中所有字母异位词
+ * 给定一个字符串s和一个非空字符串p，找到s中所有是p的字母异位词的子串，返回这些子串的起始索引。
+ * <p>
+ * 字符串只包含小写英文字母，并且字符串s和 p的长度都不超过 20100。
+ * <p>
+ * 说明：
+ * <p>
+ * 字母异位词指字母相同，但排列不同的字符串。
+ * 不考虑答案输出的顺序。
+ * 示例1:
+ * <p>
+ * 输入:
+ * s: "cbaebabacd" p: "abc"
+ * <p>
+ * 输出:
+ * [0, 6]
+ * <p>
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * s: "abab" p: "ab"
+ * <p>
+ * 输出:
+ * [0, 1, 2]
+ * <p>
+ * 解释:
+ * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
+ * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
+ * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-anagrams-in-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution438 {
+
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0, valid = 0;
+        List<Integer> resultList = new ArrayList<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            //右移窗口更新操作处理
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (right - left >= p.length()) {
+                if (valid == need.size()) {
+                    resultList.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        Solution438 solution438 = new Solution438();
+        List<Integer> result = solution438.findAnagrams("cbaebabacd", "abc");
+        System.out.println(result);
+    }
+}
