2613a531042f87fc6992b45f72e3a82e45acf525
jiangwentao <jiangwentao@xylink.com>
2021-06-06 15:31:08 +0800

    leetcode：二叉搜索树练习

---
+++ LEETCODE/Solution230.java
@@ -0,0 +1,76 @@
+/**
+ * @author jiangwentao
+ * @date 2021/6/6
+ * @detail 二叉搜索树中第K小的元素
+ * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第k个最小元素（从 1 开始计数）。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,1,4,null,2], k = 1
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [5,3,6,2,4,null,null,1], k = 3
+ * 输出：3
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中的节点数为 n 。
+ * 1 <= k <= n <= 104
+ * 0 <= Node.val <= 104
+ * <p>
+ * 进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution230 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int res = 0;
+    private int rank = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return res;
+    }
+
+    private void traverse(TreeNode root, int k) {
+        //base case
+        if (root == null) {
+            return;
+        }
+        //中序遍历
+        traverse(root.left, k);
+        rank++;
+        if (rank == k) {
+            res = root.val;
+            return;
+        }
+        traverse(root.right, k);
+    }
+}
