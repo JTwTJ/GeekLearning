f88be8fd9dd146fd10143bb6873b265e4b7365d2
jiangwentao <jiangwentao@xylink.com>
2021-06-18 16:52:11 +0800

    leetcode：二叉树

--- LEETCODE/Solution103.java
+++ LEETCODE/Solution103.java
@@ -1,43 +1,37 @@
-package Sword_To_Offer;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author jiangwentao
- * @date 2021/6/8
- * @detail 从上到下打印二叉树 II
- * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
- * <p>
- * <p>
- * 例如:
- * 给定二叉树:[3,9,20,null,null,15,7],
- * <p>
- * 3
- * / \
- * 9  20
- *   /  \
- *  15   7
- * 返回其层次遍历结果：
- * <p>
+ * @date 2021/6/18
+ * @detail 二叉树的锯齿形层序遍历
+ *
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ *
+ * 例如：
+ * 给定二叉树[3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回锯齿形层序遍历如下：
+ *
  * [
- * [3],
- * [9,20],
- * [15,7]
+ *   [3],
+ *   [20,9],
+ *   [15,7]
  * ]
- * <p>
- * 提示：
- * <p>
- * 节点总数 <= 1000
- * 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
- * <p>
- * <p>
- * <p>
+ *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+ * 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Solution32_2 {
-
+public class Solution103 {
+    
     public class TreeNode {
         int val;
         TreeNode left;
@@ -54,16 +48,21 @@ public class Solution32_2 {
             return new ArrayList<>(0);
         }
         List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+        Deque<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+        int count = 1;
         while (!queue.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
+            LinkedList<Integer> temp = new LinkedList<>();
             for (int i = queue.size(); i > 0; i--) {
                 TreeNode cur = queue.poll();
                 if (cur == null) {
                     continue;
                 }
-                temp.add(cur.val);
+                if (count % 2 == 0) {
+                    temp.addLast(cur.val);
+                } else {
+                    temp.addFirst(cur.val);
+                }
                 if (cur.left != null) {
                     queue.add(cur.left);
                 }
@@ -72,7 +71,9 @@ public class Solution32_2 {
                 }
             }
             res.add(temp);
+            count++;
         }
         return res;
     }
+    
 }
